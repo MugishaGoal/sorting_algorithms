ec16bf9cbbf6d0e55d8e89f7e569951b54673563
Mugisha Goal <degaullemugisha@gmail.com>
2023-10-01 01:53:29 -0700

    A function that sorts a doubly linked list of integers in ascending order using the Insertion sort algorithm

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -2,35 +2,37 @@
 
 /**
  * insertion_sort_list - Sorts a doubly linked list of integers
- * using the insertion sort algorithm.
+ *                       using the insertion sort algorithm.
  * @list: A pointer to the head of a doubly-linked list of integers.
  *
  * Description: Prints the list after each swap.
  */
 void insertion_sort_list(listint_t **list)
 {
-	listint_t *iter, *insert, *tmp;
+	listint_t *current, *prev, *tmp;
 
 	if (list == NULL || *list == NULL || (*list)->next == NULL)
 		return;
-	for (iter = (*list)->next; iter != NULL; iter = tmp)
+	current = (*list)->next;
+	while (current != NULL)
 	{
-		tmp = iter->next;
-		insert = iter->prev;
-		while (insert != NULL && iter->n < insert->n)
+		prev = current->prev;
+		while (prev != NULL && current->n < prev->n)
 		{
-			if (insert->prev != NULL)
-				insert->prev->next = iter;
+			tmp = current->next;
+			if (tmp != NULL)
+				tmp->prev = prev;
+			prev->next = tmp;
+			current->prev = prev->prev;
+			prev->prev = current;
+			current->next = prev;
+			if (current->prev != NULL)
+				current->prev->next = current;
 			else
-				*list = iter;
-			if (iter->next != NULL)
-				iter->next->prev = insert;
-			insert->next = iter->next;
-			iter->prev = insert->prev;
-			iter->next = insert;
-			insert->prev = iter;
-			print_list((const listint_t *)*list);
+				*list = current;
+			print_list(*list);
+			prev = current->prev;
 		}
-
+		current = current->next;
 	}
 }
